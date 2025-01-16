a54c472203f0123f60d367d408c4c855217ea7ca
carlia08 <anyel.caro@myenaip.eu>
2025-01-16 09:37:59 +0100

    test

--- variabile.js
+++ variabile.js
@@ -1,4 +1,4 @@
-*/ var nomevarialbile1 = 78;
+ var nomevarialbile1 = 78;
 console.log("qui nomevariabile1 e:"+ nomevarialbile1);
 nomevarialbile1 = "ciao";
 console.log("qui nomevariabile1 e:" + nomevarialbile1);
@@ -6,7 +6,7 @@ console.log("qui nomevariabile1 e:" + nomevarialbile1);
 {
     var nomevariabile1 = 567; //obsoleto
     let nomevariabileconlet = 43;
-} */
+} 
 
 {
     var a = 23;
