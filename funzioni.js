f62458afa94d6ea7f7338d6aa205168daa58f451
carlia08 <anyel.caro@myenaip.eu>
2025-01-16 09:37:37 +0100

    funzioni e variabili

---
+++ funzioni.js
@@ -0,0 +1,5 @@
+function saluto(){
+    alert("ciao 3c");
+}
+
+saluto();//chiamata alla funzione
---
+++ variabile.js
@@ -0,0 +1,24 @@
+*/ var nomevarialbile1 = 78;
+console.log("qui nomevariabile1 e:"+ nomevarialbile1);
+nomevarialbile1 = "ciao";
+console.log("qui nomevariabile1 e:" + nomevarialbile1);
+
+{
+    var nomevariabile1 = 567; //obsoleto
+    let nomevariabileconlet = 43;
+} */
+
+{
+    var a = 23;
+}
+
+{
+    let a = "ciao";
+}
+
+console.log(a); //???
+
+const pigreco = 3.14
+console.log("pi:" + pigrevo);
+
+pigreco = 3.15;
